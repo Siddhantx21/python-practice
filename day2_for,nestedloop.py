35ee64b320763da80f961f737cf6edc63ac49059
Siddhantx21 <siddhant21x@gmail.com>
2026-03-23 21:13:21 +0530

    Add nested loop for team members and fruits

--- day2_for,nestedloop.py
+++ day2_for,nestedloop.py
@@ -1,3 +1,4 @@
+code1:
 team = ["Siddhant - ML", "Friend 1 - Coder", "Friend 2 - Electronics"]
 
 print("\n=== MY DREAM TEAM ===")
@@ -5,3 +6,11 @@ for member in team:
     print("\nMember:", member)
     for char in member:  # nested loop to go through each character
         print("-", char)
+
+
+
+
+code2:
+fruits = ["apple banana mango"]
+for char in fruits:
+  print(char)
